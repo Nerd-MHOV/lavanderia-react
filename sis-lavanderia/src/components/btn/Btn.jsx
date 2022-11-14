49b942744610160be2c1752ce690a5b9e2694c56
Matheus Henrique <matheus.henrique4245@gmail.com>
2022-11-14 17:58:04 -0300

    remove desnecessary

--- sis-lavanderia/src/components/btn/Btn.jsx
+++
@@ -1,9 +0,0 @@
-import "./btn.scss"
-
-const Btn = ({color, action, onClick = function(){return null} }) => {
-    return (
-        <button onClick={onClick} className={`btn ${color}`}>{action}</button>
-    )
-}
-
-export default Btn
